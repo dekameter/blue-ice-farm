bfe3285b39df0e71b154115c0dc8231c0734a9ba
Dekameter <dekameter@giant.ink>
2024-11-29 00:35:17 -0700

    Fixed typo in description

--- ice.py
+++ ice.py
@@ -203,7 +203,7 @@ def main():
 Simulates the generation of a Minecraft ice farm and outputs a
 comma-separated line for each size, minimum duration, maximum duration,
 average duration, and median duration (all in ticks). If the size is
-greater than 7, than an X pattern is used for the water sources;
+greater than 7, then an X pattern is used for the water sources;
 otherwise, only a single diagnal is used.
 """,
         epilog="""
